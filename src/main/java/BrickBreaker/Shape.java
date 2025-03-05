fc4fff54ec476ca0c34024b8fe18f764b4f43a3c
pear_c <zxcv9676@naver.com>
2025-03-05 17:32:42 +0900

    Create Abstract Class Shape

---
+++ src/main/java/BrickBreaker/Shape.java
@@ -0,0 +1,22 @@
+package BrickBreaker;
+
+public abstract class Shape {
+    protected double x, y;    // 중심 좌표
+
+    public Shape(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+
+    public abstract double getMinX();
+    public abstract double getMaxX();
+    public abstract double getMinY();
+    public abstract double getMaxY();
+}
