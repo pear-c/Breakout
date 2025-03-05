023ca547e367394f0fbce0fbab8cfa6301cd1030
pear_c <zxcv9676@naver.com>
2025-03-05 17:39:37 +0900

    Refactoring - Extends Brick Classes (Shape > Rectangle > Brick)

--- src/main/java/BrickBreaker/Brick.java
+++ src/main/java/BrickBreaker/Brick.java
@@ -3,20 +3,13 @@ package BrickBreaker;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-public class Brick {
-    private double x; // 벽돌의 x 좌표
-    private double y; // 벽돌의 y 좌표
-    private double width; // 벽돌의 너비
-    private double height; // 벽돌의 높이
+public class Brick extends Rectangle{
     private Color color; // 벽돌의 색상
     private boolean isDestroyed; // 벽돌이 파괴되었는지 여부
 
     // 생성자
     public Brick(double x, double y, double width, double height, Color color) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        super(x, y, width, height);
         this.color = color;
         this.isDestroyed = false; // 초기 상태는 파괴되지 않음
     }
@@ -35,16 +28,11 @@ public class Brick {
             return false; // 이미 파괴된 벽돌은 충돌하지 않음
         }
 
-        double ballX = ball.getX();
-        double ballY = ball.getY();
-        double ballRadius = ball.getRadius();
-
         // 공이 벽돌의 경계와 충돌했는지 확인
-        boolean collision =
-                ballX + ballRadius > x &&
-                        ballX - ballRadius < x + width &&
-                        ballY + ballRadius > y &&
-                        ballY - ballRadius < y + height;
+        boolean collision = ball.getMaxX() > getMinX() &&
+                            ball.getMinX() < getMaxX() &&
+                            ball.getMaxY() > getMinY() &&
+                            ball.getMinY() < getMaxY();
 
         if (collision) {
             isDestroyed = true; // 벽돌 파괴
@@ -57,21 +45,4 @@ public class Brick {
     public boolean isDestroyed() {
         return isDestroyed;
     }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public double getHeight() {
-        return height;
-    }
-
 }
