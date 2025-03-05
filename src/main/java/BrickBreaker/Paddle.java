0745591011c894ff09c6aecd402ce934f584a8de
pear_c <zxcv9676@naver.com>
2025-03-05 17:42:38 +0900

    Refactoring - Extends Paddle Class (Shape > Rectangle > Paddle)

--- src/main/java/BrickBreaker/Paddle.java
+++ src/main/java/BrickBreaker/Paddle.java
@@ -3,20 +3,13 @@ package BrickBreaker;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-public class Paddle {
-    private double x; // 패들의 중심 x 좌표
-    private double y; // 패들의 중심 y 좌표
-    private double width; // 패들의 너비
-    private double height; // 패들의 높이
+public class Paddle extends Rectangle {
     private double speed; // 패들의 이동 속도
     private Color color; // 패들의 색상
 
     // 생성자
     public Paddle(double x, double y, double width, double height, double speed, Color color) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
+        super(x, y, width, height);
         this.speed = speed;
         this.color = color;
     }
@@ -24,7 +17,7 @@ public class Paddle {
     // 패들을 그리는 메서드
     public void draw(GraphicsContext gc) {
         gc.setFill(color);
-        gc.fillRect(x - width / 2, y - height / 2, width, height); // 중심을 기준으로 사각형 그리기
+        gc.fillRect(getMinX(), getMinY(), width, height); // 중심을 기준으로 사각형 그리기
     }
 
     // 패들의 위치를 왼쪽으로 이동
@@ -39,27 +32,22 @@ public class Paddle {
 
     // 패들이 화면 경계를 벗어나지 않도록 제한
     public void checkBounds(double canvasWidth) {
-        if (x - width / 2 < 0) { // 왼쪽 경계
+        if (getMinX() < 0) { // 왼쪽 경계
             x = width / 2;
-        } else if (x + width / 2 > canvasWidth) { // 오른쪽 경계
+        } else if (getMaxX() > canvasWidth) { // 오른쪽 경계
             x = canvasWidth - width / 2;
         }
     }
 
-    // Getter와 Setter (필요 시 사용)
-    public double getX() {
-        return x;
-    }
+    // 공과 충돌 여부 확인
+    public boolean checkCollision(Ball ball) {
+        double ballX = ball.getX();
+        double ballY = ball.getY();
+        double ballRadius = ball.getRadius();
 
-    public double getY() {
-        return y;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public double getHeight() {
-        return height;
+        return ball.getMaxY() >= getMinY() &&
+                ball.getMinY() < getMinY() &&
+                ball.getMaxX() > getMinX() &&
+                ball.getMinX() < getMaxX();
     }
 }
